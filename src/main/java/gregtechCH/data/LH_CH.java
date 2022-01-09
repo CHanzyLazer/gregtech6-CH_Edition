02cd5c20c39e91dd3c70764b3575c210b02e3ce0
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2022-01-09 17:08:56 +0800

    steam engine

--- src/main/java/gregtechCH/data/LH_CH.java
+++ src/main/java/gregtechCH/data/LH_CH.java
@@ -3,5 +3,10 @@ package gregtechCH.data;
 import gregapi.data.LH;
 
 public class LH_CH {
-    public static final String getToolTipEfficiencySimple(long aEfficiency) {aEfficiency = Math.abs(aEfficiency); return LH.percent(aEfficiency) + "%";}
+    public static final String
+            TOOLTIP_PREHEAT = "gtch.lang.preheat" // WIP
+            ;
+
+    public static final String percentSimple(long aNumber) {return String.valueOf(aNumber/100);}
+    public static final String getToolTipEfficiencySimple(long aEfficiency) {aEfficiency = Math.abs(aEfficiency); return percentSimple(aEfficiency) + "%";}
 }
